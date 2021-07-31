dbcf488593d634e2bc065af44eb729203d99ba46
Kevin Devlin <azwethinkweizkd@gmail.com>
2021-07-31 14:58:18 -0600

    Filled models to define columns

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,7 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+SELECT * FROM category;
+SELECT * FROM product;
+SELECT * FROM product_tag;
+SELECT * FROM tag
